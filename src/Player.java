115f68476763767a235b30fb65c45aca1dbca76f
bdeining <bdeining@email.arizona.edu>
2014-02-05 17:43:48 -0700

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,14 @@
 public class Player {
 
 	private String name;
+	// GET / SET
+	public int getHealth() {
+		return health;
+	}
 
+	public Player getOwner() {
+		return owner;
+	}
 	public Player(String alias) {
 		name = alias;
 
