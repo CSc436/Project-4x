fbdaa2f5e9b8fc5c96caca94205f5ef8d72e9538
kelseyyoung <kelseyyoung4@gmail.com>
2014-02-06 12:07:51 -0700

    Fullscreen canvas and resizing capability Merged with Gabe's code to display rock picture

--- src/com/client/_x.java
+++ src/com/client/_x.java
@@ -38,35 +38,48 @@ import com.googlecode.gwtgl.binding.WebGLUniformLocation;
  */
 public class _x implements EntryPoint {
 	private WebGLRenderingContext glContext;
-    private WebGLProgram shaderProgram;
-    private WebGLTexture texture;
-    private int vertexPositionAttribute, vertexTexCoordAttrib;
-    private WebGLBuffer vertexBuffer, texCoordBuffer;
-    
-    public void onModuleLoad() {
-    		final Canvas webGLCanvas = Canvas.createIfSupported();
-           
-    		RootPanel.get("gwtGL").add(webGLCanvas);
-    		webGLCanvas.setCoordinateSpaceHeight(webGLCanvas.getParent().getOffsetHeight());
-    		webGLCanvas.setCoordinateSpaceWidth(webGLCanvas.getParent().getOffsetWidth());
-    		glContext = (WebGLRenderingContext) webGLCanvas
-    				.getContext("experimental-webgl");
-    		if (glContext == null) {
-    			Window.alert("Sorry, your browser doesn't support WebGL!");
-    		}
-    		glContext.viewport(0, 0, webGLCanvas.getParent().getOffsetHeight(), webGLCanvas.getParent().getOffsetWidth());
-    		Window.addResizeHandler(new ResizeHandler() {
-    			@Override
-    			public void onResize(ResizeEvent e) {
-    				webGLCanvas.setCoordinateSpaceHeight(webGLCanvas.getParent().getOffsetHeight());
-    				webGLCanvas.setCoordinateSpaceWidth(webGLCanvas.getParent().getOffsetWidth());
-    				glContext.viewport(0, 0, webGLCanvas.getParent().getOffsetHeight(), webGLCanvas.getParent().getOffsetWidth());
-    			}
-    		});
-    		start();
-    }
-    
-    
+	private WebGLProgram shaderProgram;
+	private WebGLTexture texture;
+	private int vertexPositionAttribute, vertexTexCoordAttrib;
+	private WebGLBuffer vertexBuffer, texCoordBuffer;
+
+	public void onModuleLoad() {
+		final Canvas webGLCanvas = Canvas.createIfSupported();
+
+		RootPanel.get("gwtGL").add(webGLCanvas);
+		webGLCanvas.setCoordinateSpaceHeight(webGLCanvas.getParent()
+				.getOffsetHeight());
+		webGLCanvas.setCoordinateSpaceWidth(webGLCanvas.getParent()
+				.getOffsetWidth());
+		
+		glContext = (WebGLRenderingContext) webGLCanvas
+				.getContext("experimental-webgl");
+		if (glContext == null) {
+			Window.alert("Sorry, your browser doesn't support WebGL!");
+		}
+		//This line will make the viewport fullscreen
+		/*glContext.viewport(0, 0, webGLCanvas.getParent().getOffsetHeight(),
+				webGLCanvas.getParent().getOffsetWidth());*/
+		
+		//This line makes the viewport 500x500 to display the rock picture
+		glContext.viewport(0, 0, 500, 500);
+		
+		//Resize callback
+		Window.addResizeHandler(new ResizeHandler() {
+			@Override
+			public void onResize(ResizeEvent e) {
+				webGLCanvas.setCoordinateSpaceHeight(webGLCanvas.getParent()
+						.getOffsetHeight());
+				webGLCanvas.setCoordinateSpaceWidth(webGLCanvas.getParent()
+						.getOffsetWidth());
+				glContext.viewport(0, 0, webGLCanvas.getParent()
+						.getOffsetHeight(), webGLCanvas.getParent()
+						.getOffsetWidth());
+			}
+		});
+		start();
+	}
+
 	private void start() {
 		initShaders();
 		glContext.clearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -75,138 +88,162 @@ public class _x implements EntryPoint {
 		glContext.depthFunc(WebGLRenderingContext.LEQUAL);
 		initBuffers();
 		initTexture();
-		//drawScene();
+		// drawScene();
 	}
-    
-    private void initTexture() {
-        texture = glContext.createTexture();
-        glContext.bindTexture(WebGLRenderingContext.TEXTURE_2D, texture);
-        glContext.texImage2D(WebGLRenderingContext.TEXTURE_2D, 0, WebGLRenderingContext.RGB, 
-       		 WebGLRenderingContext.RGB, WebGLRenderingContext.UNSIGNED_BYTE, 
-       		 ImageElement.as(getImage(ClientResources.INSTANCE.riverTexture()).getElement()));
-        glContext.texParameteri(WebGLRenderingContext.TEXTURE_2D, WebGLRenderingContext.TEXTURE_MAG_FILTER, WebGLRenderingContext.LINEAR);
-        glContext.texParameteri(WebGLRenderingContext.TEXTURE_2D, WebGLRenderingContext.TEXTURE_MIN_FILTER, WebGLRenderingContext.LINEAR);
-        glContext.bindTexture(WebGLRenderingContext.TEXTURE_2D, null);
-        //System.out.println(texture);
-    }
-    
-    public Image getImage(final ImageResource imageResource) {
-    	//System.out.println("(" + imageResource.getWidth() + ", " + imageResource.getHeight() +")");
-        final Image img = new Image();
-        img.addLoadHandler(new LoadHandler() {
-                @Override
-                public void onLoad(LoadEvent event) {
-                        RootPanel.get().remove(img);
-                        drawScene();
-                }
-        });
-        img.setVisible(false);
-        RootPanel.get().add(img);
-        
-        img.setUrl(imageResource.getSafeUri());
-        //System.out.println(img);
-        return img;
-    }
-	
-    public void initShaders() {
-        WebGLShader fragmentShader = getShader(WebGLRenderingContext.FRAGMENT_SHADER, ClientResources.INSTANCE.fragmentShader().getText());
-        WebGLShader vertexShader = getShader(WebGLRenderingContext.VERTEX_SHADER, ClientResources.INSTANCE.vertexShader().getText());
 
-        shaderProgram = glContext.createProgram();
-        glContext.attachShader(shaderProgram, vertexShader);
-        glContext.attachShader(shaderProgram, fragmentShader);
-        glContext.linkProgram(shaderProgram);
+	private void initTexture() {
+		texture = glContext.createTexture();
+		glContext.bindTexture(WebGLRenderingContext.TEXTURE_2D, texture);
+		glContext.texImage2D(
+				WebGLRenderingContext.TEXTURE_2D,
+				0,
+				WebGLRenderingContext.RGB,
+				WebGLRenderingContext.RGB,
+				WebGLRenderingContext.UNSIGNED_BYTE,
+				ImageElement.as(getImage(
+						ClientResources.INSTANCE.riverTexture()).getElement()));
+		glContext.texParameteri(WebGLRenderingContext.TEXTURE_2D,
+				WebGLRenderingContext.TEXTURE_MAG_FILTER,
+				WebGLRenderingContext.LINEAR);
+		glContext.texParameteri(WebGLRenderingContext.TEXTURE_2D,
+				WebGLRenderingContext.TEXTURE_MIN_FILTER,
+				WebGLRenderingContext.LINEAR);
+		glContext.bindTexture(WebGLRenderingContext.TEXTURE_2D, null);
+		// System.out.println(texture);
+	}
 
-        if (!glContext.getProgramParameterb(shaderProgram, WebGLRenderingContext.LINK_STATUS)) {
-                throw new RuntimeException("Could not initialise shaders");
-        }
+	public Image getImage(final ImageResource imageResource) {
+		// System.out.println("(" + imageResource.getWidth() + ", " +
+		// imageResource.getHeight() +")");
+		final Image img = new Image();
+		img.addLoadHandler(new LoadHandler() {
+			@Override
+			public void onLoad(LoadEvent event) {
+				RootPanel.get().remove(img);
+				drawScene();
+			}
+		});
+		img.setVisible(false);
+		RootPanel.get().add(img);
 
-        glContext.useProgram(shaderProgram);
+		img.setUrl(imageResource.getSafeUri());
+		// System.out.println(img);
+		return img;
+	}
 
-        vertexPositionAttribute = glContext.getAttribLocation(shaderProgram, "vertexPosition");
-        vertexTexCoordAttrib = glContext.getAttribLocation(shaderProgram, "vertexTexCoord");
-        
-        glContext.enableVertexAttribArray(vertexPositionAttribute);
-        glContext.enableVertexAttribArray(vertexTexCoordAttrib);
-    }
+	public void initShaders() {
+		WebGLShader fragmentShader = getShader(
+				WebGLRenderingContext.FRAGMENT_SHADER, ClientResources.INSTANCE
+						.fragmentShader().getText());
+		WebGLShader vertexShader = getShader(
+				WebGLRenderingContext.VERTEX_SHADER, ClientResources.INSTANCE
+						.vertexShader().getText());
 
-   	private WebGLShader getShader(int type, String source) {
-   		WebGLShader shader = glContext.createShader(type);
+		shaderProgram = glContext.createProgram();
+		glContext.attachShader(shaderProgram, vertexShader);
+		glContext.attachShader(shaderProgram, fragmentShader);
+		glContext.linkProgram(shaderProgram);
 
-   		glContext.shaderSource(shader, source);
-   		glContext.compileShader(shader);
-
-   		if (!glContext.getShaderParameterb(shader,
-   				WebGLRenderingContext.COMPILE_STATUS)) {
-   			throw new RuntimeException(glContext.getShaderInfoLog(shader));
-   		}
-
-   		return shader;
-   	}
-
-    private void initBuffers() {
-        vertexBuffer = glContext.createBuffer();
-        glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, vertexBuffer);
-        float[] vertices = new float[]{
-                         1.0f,  1.0f,  -2.5f, // first vertex
-                        -1.0f, 1.0f,  -2.5f, // second vertex
-                         1.0f, -1.0f,  -2.5f,  // third vertex
-                         -1.0f, -1.0f, -2.5f
-        };
-        glContext.bufferData(WebGLRenderingContext.ARRAY_BUFFER, Float32Array.create(vertices), WebGLRenderingContext.STATIC_DRAW);
-    
-        texCoordBuffer = glContext.createBuffer();
-        glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, texCoordBuffer);
-        float[] colors = new float[]{
-                1.0f, 0.0f, 
-                0.0f, 0.0f, 
-                1.0f, 1.0f, 
-                0.0f, 1.0f
-        };
-        glContext.bufferData(WebGLRenderingContext.ARRAY_BUFFER, Float32Array.create(colors), WebGLRenderingContext.STATIC_DRAW);
-    }
-  
-	  private void drawScene() {
-	        glContext.clear(WebGLRenderingContext.COLOR_BUFFER_BIT | WebGLRenderingContext.DEPTH_BUFFER_BIT);
-	       
-	        // create perspective matrix
-	        float[] perspectiveMatrix = createPerspectiveMatrix(45, 1, 0.1f, 1000);
-	        WebGLUniformLocation uniformLocation = glContext.getUniformLocation(shaderProgram, "perspectiveMatrix");
-	       
-	    	 // vertices
-	        glContext.uniformMatrix4fv(uniformLocation, false, perspectiveMatrix);
-	        glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, vertexBuffer);
-	        glContext.vertexAttribPointer(vertexPositionAttribute, 3, WebGLRenderingContext.FLOAT, false, 0, 0);
-	        
-	        // texture coordinates
-	        glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, texCoordBuffer);
-	        glContext.vertexAttribPointer(vertexTexCoordAttrib, 2, WebGLRenderingContext.FLOAT, false, 0, 0);
-	       
-	        // texture data
-	        glContext.activeTexture(glContext.TEXTURE0);
-	        glContext.bindTexture(glContext.TEXTURE_2D, texture);
-	        glContext.uniform1i(glContext.getUniformLocation(shaderProgram,  "texture"), 0);
-	        
-	        // draw geometry
-	        glContext.drawArrays(WebGLRenderingContext.TRIANGLE_STRIP, 0, 4);
-	        glContext.flush();
-	    }
-	  
-	  private float[] createPerspectiveMatrix(int fieldOfViewVertical, float aspectRatio, float minimumClearance, float maximumClearance) {
-	        float top    = minimumClearance * (float)Math.tan(fieldOfViewVertical * Math.PI / 360.0);
-	        float bottom = -top;
-	        float left   = bottom * aspectRatio;
-	        float right  = top * aspectRatio;
-
-	        float X = 2*minimumClearance/(right-left);
-	        float Y = 2*minimumClearance/(top-bottom);
-	        float A = (right+left)/(right-left);
-	        float B = (top+bottom)/(top-bottom);
-	        float C = -(maximumClearance+minimumClearance)/(maximumClearance-minimumClearance);
-	        float D = -2*maximumClearance*minimumClearance/(maximumClearance-minimumClearance);
-
-			return new float[] { X, 0.0f, A, 0.0f, 0.0f, Y, B, 0.0f, 0.0f, 0.0f, C,
-					-1.0f, 0.0f, 0.0f, D, 0.0f };
+		if (!glContext.getProgramParameterb(shaderProgram,
+				WebGLRenderingContext.LINK_STATUS)) {
+			throw new RuntimeException("Could not initialise shaders");
 		}
-}
 
+		glContext.useProgram(shaderProgram);
+
+		vertexPositionAttribute = glContext.getAttribLocation(shaderProgram,
+				"vertexPosition");
+		vertexTexCoordAttrib = glContext.getAttribLocation(shaderProgram,
+				"vertexTexCoord");
+
+		glContext.enableVertexAttribArray(vertexPositionAttribute);
+		glContext.enableVertexAttribArray(vertexTexCoordAttrib);
+	}
+
+	private WebGLShader getShader(int type, String source) {
+		WebGLShader shader = glContext.createShader(type);
+
+		glContext.shaderSource(shader, source);
+		glContext.compileShader(shader);
+
+		if (!glContext.getShaderParameterb(shader,
+				WebGLRenderingContext.COMPILE_STATUS)) {
+			throw new RuntimeException(glContext.getShaderInfoLog(shader));
+		}
+
+		return shader;
+	}
+
+	private void initBuffers() {
+		vertexBuffer = glContext.createBuffer();
+		glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, vertexBuffer);
+		float[] vertices = new float[] { 1.0f, 1.0f, -2.5f, // first vertex
+				-1.0f, 1.0f, -2.5f, // second vertex
+				1.0f, -1.0f, -2.5f, // third vertex
+				-1.0f, -1.0f, -2.5f };
+		glContext.bufferData(WebGLRenderingContext.ARRAY_BUFFER,
+				Float32Array.create(vertices),
+				WebGLRenderingContext.STATIC_DRAW);
+
+		texCoordBuffer = glContext.createBuffer();
+		glContext
+				.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, texCoordBuffer);
+		float[] colors = new float[] { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+				0.0f, 1.0f };
+		glContext.bufferData(WebGLRenderingContext.ARRAY_BUFFER,
+				Float32Array.create(colors), WebGLRenderingContext.STATIC_DRAW);
+	}
+
+	private void drawScene() {
+		glContext.clear(WebGLRenderingContext.COLOR_BUFFER_BIT
+				| WebGLRenderingContext.DEPTH_BUFFER_BIT);
+
+		// create perspective matrix
+		float[] perspectiveMatrix = createPerspectiveMatrix(45, 1, 0.1f, 1000);
+		WebGLUniformLocation uniformLocation = glContext.getUniformLocation(
+				shaderProgram, "perspectiveMatrix");
+
+		// vertices
+		glContext.uniformMatrix4fv(uniformLocation, false, perspectiveMatrix);
+		glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, vertexBuffer);
+		glContext.vertexAttribPointer(vertexPositionAttribute, 3,
+				WebGLRenderingContext.FLOAT, false, 0, 0);
+
+		// texture coordinates
+		glContext
+				.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, texCoordBuffer);
+		glContext.vertexAttribPointer(vertexTexCoordAttrib, 2,
+				WebGLRenderingContext.FLOAT, false, 0, 0);
+
+		// texture data
+		glContext.activeTexture(glContext.TEXTURE0);
+		glContext.bindTexture(glContext.TEXTURE_2D, texture);
+		glContext.uniform1i(
+				glContext.getUniformLocation(shaderProgram, "texture"), 0);
+
+		// draw geometry
+		glContext.drawArrays(WebGLRenderingContext.TRIANGLE_STRIP, 0, 4);
+		glContext.flush();
+	}
+
+	private float[] createPerspectiveMatrix(int fieldOfViewVertical,
+			float aspectRatio, float minimumClearance, float maximumClearance) {
+		float top = minimumClearance
+				* (float) Math.tan(fieldOfViewVertical * Math.PI / 360.0);
+		float bottom = -top;
+		float left = bottom * aspectRatio;
+		float right = top * aspectRatio;
+
+		float X = 2 * minimumClearance / (right - left);
+		float Y = 2 * minimumClearance / (top - bottom);
+		float A = (right + left) / (right - left);
+		float B = (top + bottom) / (top - bottom);
+		float C = -(maximumClearance + minimumClearance)
+				/ (maximumClearance - minimumClearance);
+		float D = -2 * maximumClearance * minimumClearance
+				/ (maximumClearance - minimumClearance);
+
+		return new float[] { X, 0.0f, A, 0.0f, 0.0f, Y, B, 0.0f, 0.0f, 0.0f, C,
+				-1.0f, 0.0f, 0.0f, D, 0.0f };
+	}
+}
