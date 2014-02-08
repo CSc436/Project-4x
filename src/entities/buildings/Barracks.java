52e7b8cc0e472daefb5a5ca7f1d13eab82ec48be
bdeining <bdeining@harmonica.cs.arizona.edu>
2014-02-08 14:53:21 -0700

    asd

---
+++ src/entities/buildings/Barracks.java
@@ -0,0 +1,15 @@
+package entities.buildings;
+
+import entities.Player;
+
+public class Barracks extends Building {
+
+	
+	
+	
+	public Barracks(Player p, int h, int w) {
+		super(p, h, w);
+
+	}
+
+}
