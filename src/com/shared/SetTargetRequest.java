20dfeccff01cbdf8aff3c4871ee12fef3a1a0776
stopping <stopping@email.arizona.edu>
2014-03-01 16:15:16 -0700

    Unit movement demo for front end

---
+++ src/com/shared/SetTargetRequest.java
@@ -0,0 +1,31 @@
+package com.shared;
+
+import com.server.Model;
+
+public class SetTargetRequest extends Request {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4338389460462282941L;
+	double x;
+	double y;
+	
+	public SetTargetRequest() {
+		super();
+		this.x = 0.0;
+		this.y = 0.0;
+	}
+	
+	public SetTargetRequest(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	public boolean executeOn(Model m) {
+		m.setTarget(x, y);
+		return true;
+	}
+
+}
