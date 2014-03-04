699db4479349e0c1d617e2011f71a4c1a3a2d318
Ben Whitely <whitely@email.arizona.edu>
2014-03-03 19:11:55 -0700

    Entered enough skeleton code so the approach makes sense.

--- src/com/server/Dispatch.java
+++ src/com/server/Dispatch.java
@@ -1,8 +1,7 @@
 package com.server;
 
-import java.util.Vector;
-
 import control.Player;
+import entities.units.Agent;
 
 public class Dispatch implements ModelEventListener {
 	
@@ -19,6 +18,29 @@ public class Dispatch implements ModelEventListener {
 	@Override
 	public void onModelEvent(ModelEvent me) {
 		// TODO Auto-generated method stub
+		switch (me.type) {
+		case UNIT:
+			
+			Agent a = (Agent)me.getPayload();
+			
+			//Now add a to hash maps.
+			
+			//Now determine who should be notified about A.
+			
+			//Now add a to the appropriate notification queues.
+			
+			//TODO: Implement these things.
+			
+			break;
+		case BUILDING:
+			
+			break;
+			
+		default:
+			throw new IllegalArgumentException("A valid Target was not specified.");
+		}
+		
+		
 		
 	}
 }
