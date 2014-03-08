c53a43019fd7552af2e4b09b15afbdb8985a6c8f
Nicholas Topping <NRTopping@email.arizona.edu>
2014-03-08 15:55:47 -0700

    Added documentation to Agent.java, stating parameters of constructor.

--- src/entities/units/Agent.java
+++ src/entities/units/Agent.java
@@ -24,15 +24,23 @@ public abstract class Agent extends Unit {
 	private Map<String, Unit> underlings; // TODO possibly replace Unit or String with their unique ID's (UUID?)
 	private CommandQueue cq; // list of current commnands to issue.
 
-	/*
+	/**
 	 * Agent():
 	 * Description:
 	 * Base constructor for and Agent object. Calls the Unit constructor, 
 	 * initializes underlings, and Command Queue. 
 	 * 
 	 * Parameters:
+	 * @param UUID id - a unique id created by Java's UUID class
+	 * @param int playerId - id of player that owns this agent
+	 * @param BaseStatsEnum baseStats - base stats of a unit (Baseline for unit, if unit has no research/improvements, then unitstats = baseStats) 
+	 * @param UnitStats new_stats - the altered stats of the unit (will be used for combat/gameplay) 
+	 * @param GameObjectType type - the group that this object applies to (i.e. general groups such as building, unit, blah) 
+	 * @param UnitType unitTYpe - more specific type within the group
+	 * @param float xco - initial x coordinate of the unit
+	 * @param float yco - initial y coordinate of the unit 
+	 * 
 	 */
-	
 	public Agent(UUID id, int playerId, BaseStatsEnum baseStats, UnitStats new_stats, GameObjectType type, UnitType unitType, float xco,
 			float yco) {
 		super(id, playerId,  baseStats, new_stats, type, unitType, xco, yco);
