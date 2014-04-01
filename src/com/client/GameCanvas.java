d09e5140ee1f38d2aeeba1949955177c06308786
Gabe Kishi <gtkishi@email.arizona.edu>
2014-03-31 18:11:26 -0700

    KELSEY IS A BIG JERK!

--- src/com/client/GameCanvas.java
+++ src/com/client/GameCanvas.java
@@ -64,7 +64,7 @@ public class GameCanvas {
 			right = false, left = false, rotateLeft = false, rotateRight = false, center = false;
 	private long time;
 
-	public static final int GRID_WIDTH = 16;
+	public static final int GRID_WIDTH = 256;
 	private final int NUM_TILES = GRID_WIDTH * GRID_WIDTH;
 	private final boolean debug = true;
 
