7f8015dea393d0cb0027ef1795934f8b2a5ef4b1
stopping <stopping@email.arizona.edu>
2014-04-09 12:57:13 -0700

    Random change

--- src/com/shared/MyFactory.java
+++ src/com/shared/MyFactory.java
@@ -4,6 +4,9 @@ import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanFactory;
 
 public interface MyFactory extends AutoBeanFactory {
+	/*
+	 * Need to implement interfaces for all sendable/receivable objects
+	 */
 	AutoBean<SimpleGameModel> sgm();
 	
 	AutoBean<SimpleGameModel> sgm(SimpleGameModel model);
