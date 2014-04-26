b812ab1c58fe021991597c07c627f12f772d3195
kelseyyoung <kelseyyoung4@gmail.com>
2014-04-26 15:08:38 -0700

    Loading screen done

--- src/com/client/PlayGame.java
+++ src/com/client/PlayGame.java
@@ -1,26 +1,44 @@
 package com.client;
+
 import static com.google.gwt.query.client.GQuery.$;
 
-import java.util.Random;
-
+import com.client.gameinterface.Console;
 import com.client.gameinterface.GameInterface;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.query.client.Function;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 
-
 public class PlayGame implements EntryPoint {
 
-	
+
 	public void onModuleLoad() {
-		
-		ClientModel theModel = new ClientModel();
-		theModel.run();
-		GameCanvas canvas = new GameCanvas(theModel);
-		GameInterface.init();
-		// Remove loading screen
-		$("#loading-screen").remove();
+
+		$("#login-button").click(new Function() {
+			public boolean f(Event e) {
+				Console.log($("#login-username").val());
+				$("#login-screen").remove();
+				// Init game in 1 second
+				// (This is purely to see the loading screen)
+				Timer t = new Timer() {
+					@Override
+					public void run() {
+						ClientModel theModel = new ClientModel();
+						theModel.run();
+						GameCanvas canvas = new GameCanvas(theModel);
+						GameInterface.init();
+						// Remove loading screen
+						$("#loading-screen").remove();
+					}
+				};
+				t.schedule(1000);
+				return true;
+			}
+		});
 	}
+
 }
