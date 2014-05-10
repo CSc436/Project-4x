4e7d486ed787f28994896cd28415ab7d7188050e
Nicholas Topping <NRTopping@email.arizona.edu>
2014-05-10 15:31:52 -0700

    Added ability to press enter from username or password text box to loggin, got tired of having to click the button >_>

--- src/com/client/PlayGame.java
+++ src/com/client/PlayGame.java
@@ -24,41 +24,69 @@ public class PlayGame implements EntryPoint {
 		
 		$("#login-button").click(new Function() {
 			public boolean f(Event e) {
-				Console.log($("#login-username").val());
-				Console.log($("#login-password").val());
-
-				// Added quick hack to get loggin name to be chat name - Nick
-				// Possibly move to run method? 
-				GameInterface.setPlayerName($("#login-username").val());
-				
-				Window.alert("Login?!");
-				// Remove login screen
-				$("#login-screen").remove();
-				
-				// Init game in 1 second
-				// (This is purely to see the loading screen)
-				Timer t = new Timer() {
-					@Override
-					public void run() {
-						// Set actions on loading screen
-						$("#loading-actions").html("INITIALIZING MODEL");
-						ClientController theModel = new ClientController();
-						theModel.run();
-						// Set actions on loading screen
-						$("#loading-actions").html("SETTING CANVAS");
-						GameCanvas canvas = new GameCanvas(theModel);
-						GameInterface.init(theModel, canvas);
-						// Set actions on loading screen
-						$("#loading-actions").html("RETRIEVING GAME STATE");
-						/** NOTE: loading screen now gets removed once the game model is set **/
-						/** Which happens in ClientModel.java **/
-					}
-				};
-				// Schedule the dummy timer
-				t.schedule(1000);
-				return true;
+				return play();
 			}
 		});
+		
+		// Allow use to press enter to loggin, that way we can test slightly faster
+		$("#login-username").keypress(new Function() {
+			public boolean f (Event e)
+			{
+				if (e.getKeyCode() == 13) 
+				{
+					return play();
+				}
+				return false;
+			}
+		});
+		// Allow use to press enter to loggin, that way we can test slightly faster
+		$("#login-password").keypress(new Function() {
+			public boolean f (Event e)
+			{
+				if (e.getKeyCode() == 13) 
+				{
+					return play();
+				}
+				return false;
+			}
+		});
+		
 	}
 
+	private boolean play()
+	{
+		Console.log($("#login-username").val());
+		Console.log($("#login-password").val());
+
+		// Added quick hack to get loggin name to be chat name - Nick
+		// Possibly move to run method? 
+		GameInterface.setPlayerName($("#login-username").val());
+		
+		Window.alert("Login?!");
+		// Remove login screen
+		$("#login-screen").remove();
+		
+		// Init game in 1 second
+		// (This is purely to see the loading screen)
+		Timer t = new Timer() {
+			@Override
+			public void run() {
+				// Set actions on loading screen
+				$("#loading-actions").html("INITIALIZING MODEL");
+				ClientController theModel = new ClientController();
+				theModel.run();
+				// Set actions on loading screen
+				$("#loading-actions").html("SETTING CANVAS");
+				GameCanvas canvas = new GameCanvas(theModel);
+				GameInterface.init(theModel, canvas);
+				// Set actions on loading screen
+				$("#loading-actions").html("RETRIEVING GAME STATE");
+				/** NOTE: loading screen now gets removed once the game model is set **/
+				/** Which happens in ClientModel.java **/
+			}
+		};
+		// Schedule the dummy timer
+		t.schedule(1000);
+		return true;
+	}
 }
