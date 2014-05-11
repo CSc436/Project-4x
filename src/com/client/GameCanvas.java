24acd9ec319e8bd0e6d168a0899155a94f23f292
Sean <stopping@email.arizona.edu>
2014-05-11 16:41:01 -0700

    Barracks model added to game, merged from Alex's branch.

--- src/com/client/GameCanvas.java
+++ src/com/client/GameCanvas.java
@@ -218,13 +218,6 @@ public class GameCanvas {
 		
 		final Mesh barracks1 = OBJImporter.objToMesh(ClientResources.INSTANCE.barracksOBJ().getText(), glContext);
 		barracks1.setTexture(glContext, ClientResources.INSTANCE.barracksTexture());
-		barracks1.posX = 6.5f;
-		barracks1.posY = 5.5f;
-		barracks1.posZ = 0.0f;
-		barracks1.id = 17261;
-		barracks1.healthPercentage = 0.99f;
-		barracks1.setTeamColor(0.0f,1.0f,0.0f);
-		entities.put(barracks1.id, barracks1);
 		
 		/*// STRAIN THE SERVER AAAAAAHHHHH 
 		int idcount = 300;
@@ -250,6 +243,7 @@ public class GameCanvas {
 		for(BuildingType t : BuildingType.values()) {
 			buildingMeshes.put(t, castle1);
 		}
+		buildingMeshes.put(BuildingType.BARRACKS, barracks1);
 		
 	}
 	
